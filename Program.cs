bcfedcd2c72d25e5600d924ca34b4435e39e6787
Brennan Harrison <brennan16@usf.edu>
2020-02-04 17:29:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Programmer: Brennan Harrison
+// Date: 02/24/2020
+// Project: Class Project Deliverable 3- Arrays.
+
+using System;
+
+namespace ArrayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a message to the console.
+            Console.WriteLine("This program will iterate through a loop!");
+
+            // Integer arrary.
+            int[] arrayName = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            // Iterate through each element in the array.
+            foreach (int i in arrayName)
+            {
+                // Write a message to the console.
+                Console.WriteLine("Element value = " + i);
+            }
+        }
+    }
+}
